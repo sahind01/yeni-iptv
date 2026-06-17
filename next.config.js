fcb6ab78827da368d0feefa283d41fb54063f6db
sahind01 <164308037+sahind01@users.noreply.github.com>
2026-06-18 00:00:10 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -17,9 +10,6 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
