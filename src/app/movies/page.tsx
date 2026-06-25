2560ce25665e3140e8392b497d4d6b962ed674f6
sahind01 <164308037+sahind01@users.noreply.github.com>
2026-06-25 18:35:49 +0300

    Replace VideoPlayer import with VodPlayer

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import MainLayout from '@/components/Layout/MainLayout';
-import VideoPlayer from '@/components/Player/VideoPlayer';
+import VideoPlayer from '@/components/Player/VodPlayer';
 import { useStore } from '@/store/useStore';
 import { FiFilm, FiSearch, FiX, FiGrid, FiList, FiPlay, FiLoader } from 'react-icons/fi';
 import { motion } from 'framer-motion';
